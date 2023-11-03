cf0ab2f1c0025a7004910ec36eab00492a85f3de
rootel <ckcksdl135@naver.com>
2023-11-03 17:58:33 +0900

    quotes

--- main.js
+++ main.js
@@ -12,3 +12,54 @@ function getTime() {
 
 getTime();
 setInterval(getTime, 1000);
+
+function getQuotes() {
+  const quotes = document.querySelector(".quotes");
+
+  let savedQuotes = localStorage.getItem("quotesList");
+
+  if (!savedQuotes) {
+    //없으면 기본적으로 하나 생성
+    localStorage.setItem("quotesList", JSON.stringify(["Roopretelcham"]));
+
+    savedQuotes = localStorage.getItem("quotesList");
+  }
+
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  quotes.innerText =
+    parsedQuotes[Math.floor(Math.random() * parsedQuotes.length)];
+}
+
+getQuotes();
+
+function onClickNewQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuotes = document.querySelector(".new-quotes");
+  const newQuotesInput = document.querySelector(".new-quotes-input");
+
+  if (!newQuotesInput.value) return;
+
+  // 로컬 스토리지에 저장
+  let savedQuotes = localStorage.getItem("quotesList");
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  parsedQuotes.push(newQuotesInput.value);
+
+  localStorage.setItem("quotesList", JSON.stringify(parsedQuotes));
+
+  // 현재 페이지 반영
+  quotes.innerText = newQuotesInput.value;
+  newQuotesInput.value = "";
+
+  quotes.style.display = "block";
+  newQuotes.style.display = "none";
+}
+
+function onClickQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuotes = document.querySelector(".new-quotes");
+
+  quotes.style.display = "none";
+  newQuotes.style.display = "block";
+}
